6c3deea0971da228143d13df0dac2b731b0d8aaa
RavshanMVP <93672457+RavshanMVP@users.noreply.github.com>
2022-05-25 15:25:15 +0500

    Delete homework-5/Company directory

--- homework-5/Company/api/urls/service.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-from api.view import ServiceDetails
-
-urlpatterns = [
-    path("<pk>", ServiceDetails.as_view({"get":'retrieve','delete':'delete','put':'put','list':'list','post':'post'})),
-            path("list/", ServiceDetails.as_view({'get':'list'})),
-]
